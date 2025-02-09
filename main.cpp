97da015dcbe5b3d7c3ebd1ef7a5de621131f59d0
Artem Novorosiuk <artem.novorosiuk@student.kdg.be>
2025-02-09 15:17:00 +0100

    order dictionary + ability to delete order

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <format>
 #include <iostream>
 #include <map>
+#include <memory>
+#include <vector>
 
 using Quantity = size_t;
 using Price = double;
@@ -65,53 +67,90 @@ public:
 
 };
 
-class OrderManager
-{
-private:
-    OrderID counter = 0;
-
-public:
-    Order createOrder(Price price, Quantity quantity, Side side, OrderType type)
-    {
-        return {++counter,price,quantity,side,type};
-    }
-
-};
+using OrderDictionary = std::vector<std::shared_ptr<Order>>;
 
 class OrderBook
 {
 private:
     OrderID counter = 0;
 
-    std::multimap<Price, std::deque<Order>, std::greater<>> buys { };
-    std::multimap<Price, std::deque<Order>, std::less<>> bids { };
+    std::multimap<Price, std::deque<std::shared_ptr<Order>>, std::greater<>> buys { };
+    std::multimap<Price, std::deque<std::shared_ptr<Order>>, std::less<>> bids { };
+
+    OrderDictionary order_dictionary {};
+
 public:
 
     OrderBook() = default;
 
-    void orderPlace(const Order& order)
+    std::shared_ptr<Order> orderPlace(Price price, Quantity quantity, Side side, OrderType type)
     {
-        if (order.getSide() == Side::Buy)
+        auto order = std::make_shared<Order>(++counter,price,quantity,side,type);
+        order_dictionary.push_back(order);
+
+        if (order->getSide() == Side::Buy)
         {
-            if (const auto it = buys.find(order.getPrice()); it == buys.end())
+            if (const auto it = buys.find(order->getPrice()); it == buys.end())
             {
-                buys.insert({order.getPrice(), std::deque{order}});
+                buys.insert({order->getPrice(), std::deque{order}});
             } else
             {
                 it->second.push_back(order);
             }
         }
-        if (order.getSide() == Side::Sell)
+        if (order->getSide() == Side::Sell)
         {
-            if (const auto it = bids.find(order.getPrice()); it == bids.end())
+            if (const auto it = bids.find(order->getPrice()); it == bids.end())
             {
-                bids.insert({order.getPrice(), std::deque{order}});
+                bids.insert({order->getPrice(), std::deque{order}});
             } else
             {
                 it->second.push_back(order);
             }
         }
+        return order;
     }
+
+    void orderDelete(OrderID order_id)
+    {
+        const auto it = std::ranges::find_if(order_dictionary, [order_id] (const std::shared_ptr<Order>& order)
+        {
+            return order->getID() == order_id;
+        });
+
+        if (it == order_dictionary.end())
+        {
+            throw std::invalid_argument("Order does not exist");
+            return;
+        }
+        const auto& order = *it;
+
+        order_dictionary.erase(it);
+
+        if (order->getSide() == Side::Buy)
+        {
+            const auto mapsIt = buys.find(order->getPrice());
+            auto& dq = mapsIt->second;
+            std::erase(dq, order);
+
+            if (dq.empty())
+            {
+                buys.erase(mapsIt);
+            }
+        } else
+        {
+            const auto mapsIt = bids.find(order->getPrice());
+            auto& dq = mapsIt->second;
+            std::erase(dq, order);
+
+            if (dq.empty())
+            {
+                bids.erase(mapsIt);
+            }
+        }
+
+    }
+
     void printQuantityAtLevel(const Price price)
     {
         if (const auto it = buys.find(price); it == buys.end())
@@ -121,10 +160,10 @@ public:
         } else
         {
             Quantity quan = 0;
-            for (const auto orders = it->second; const auto order : orders)
+            for (const auto orders = it->second; const auto& order : orders)
             {
-                std::cout << std::format("Order {} with quantity {}", order.getID(), order.getQuantity()) << std::endl;
-                quan += order.getQuantity();
+                std::cout << std::format("Order {} with quantity {}", order->getID(), order->getQuantity()) << std::endl;
+                quan += order->getQuantity();
             }
             std::cout << "Total quantity is " << quan << std::endl;
         }
@@ -138,11 +177,15 @@ int main()
 {
     OrderBook order_book {};
 
-    order_book.orderPlace(Order{1,15.32, 100, Side::Buy, OrderType::GoodTillCancel});
-    order_book.orderPlace(Order{2,15.32, 80, Side::Buy, OrderType::GoodTillCancel});
-    order_book.orderPlace(Order{3,15.33, 30, Side::Buy, OrderType::GoodTillCancel});
+    order_book.orderPlace(15, 100, Side::Buy, OrderType::GoodTillCancel);
+    order_book.orderPlace(15, 80, Side::Buy, OrderType::GoodTillCancel);
+    order_book.orderPlace(15, 30, Side::Buy, OrderType::GoodTillCancel);
 
-    order_book.printQuantityAtLevel(15.32);
+    order_book.printQuantityAtLevel(15);
+
+    order_book.orderDelete(1);
+
+    order_book.printQuantityAtLevel(15);
     return 0;
 }
 
